b08aef2b1f0e73e69f43c154a31463e99a0f71d7
Bishwas Pandey <pandeybishwas5@gmail.com>
2023-11-16 23:35:37 +1100

    Add more users

--- index.js
+++ index.js
@@ -19,24 +19,47 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let currentUserId = 1;
+
+let users = [
+  { id: 1, name: "Bishwas", color: "teal" },
+  { id: 2, name: "Bikram", color: "powderblue" },
+];
+
 
 async function checkVisisted() {
-  const result = await db.query("SELECT country_code FROM visited_countries");
+  const result = await db.query(
+    "SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1; ",
+    [currentUserId]
+  );
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
   return countries;
 }
+
+async function getCurrentUser(){
+  const result = await db.query("SELECT * FROM users");
+  users = result.rows;
+  return users.find((user) => user.id == currentUserId);
+}
 // GET home page
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
-  res.render("index.ejs", { countries: countries, total: countries.length });
+  const currentUser = await getCurrentUser();
+  res.render("index.ejs", { 
+    countries: countries, 
+    total: countries.length,
+    users: users,
+    color: currentUser.color,  
+  });
 });
 
 //INSERT new country
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
+  const currentUser = await getCurrentUser();
 
   try {
     const result = await db.query(
@@ -48,8 +71,8 @@ app.post("/add", async (req, res) => {
     const countryCode = data.country_code;
     try {
       await db.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1)",
-        [countryCode]
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        [countryCode, currentUserId]
       );
       res.redirect("/");
     } catch (err) {
@@ -70,6 +93,33 @@ app.post("/add", async (req, res) => {
       error: "Country name does not exist, try again.",
     });
   }
+});
+
+app.post("/user", async (req, res) => {
+  if (req.body.add === "new"){
+    res.render("new.ejs");
+  }else{
+    currentUserId = req.body.user;
+    res.redirect("/");
+  }
+});
+ 
+
+
+
+app.post("/new", async (req, res) => {
+  const name = req.body.name;
+  const color = req.body.color;
+
+  const result = await db.query(
+    "INSERT INTO users (name, color) VALUES ($1, $2) RETURNING *;",
+    [name, color]
+  );
+  console.log(result.rows);
+  const id = result.rows[0].id;
+  currentUserId = id;
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
